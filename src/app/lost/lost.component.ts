17cd38c56f877c128e60440eec418cc854892385
Hetavi Shah <shahhetvi990@gmail.com>
2019-02-04 07:23:20 -0600

    finished the app

--- src/app/lost/lost.component.ts
+++ src/app/lost/lost.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as algoliasearch from 'algoliasearch';
 
 
 
@@ -8,18 +9,52 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lost.component.css']
 })
 export class LostComponent implements OnInit {
+  index: any;
+  client: any;
 
-   
+    // algoliaConfig = {
+    //   apiKey: '06b8bfc492b6757949b4220017d8693d',
+    //   appId: 'K5MHKFSXEU',
+    //   indexName: 'lostAndFoundData',
+    //   routing: true,
+    // };
 
-    algoliaConfig = {
-      apiKey: '06b8bfc492b6757949b4220017d8693d',
-      appId: 'K5MHKFSXEU',
-      indexName: 'bestbuy'
-    };
+    
 
-  constructor() { }
+    txtVal : string
+    query : string;
+    results = ["hey"];
+    showResults = false;
+  constructor() {
+    this.client = algoliasearch('K5MHKFSXEU', '06b8bfc492b6757949b4220017d8693d');
+  this.index = this.client.initIndex('lostAndFoundData');
+   }
 
   ngOnInit() {
+  }
+
+  searchChanged(query) {
+    if (query.length) {
+      this.showResults = true;
+    } else {
+      this.showResults = false;
+    }
+  }
+
+  searchThisQuery(query : string) {
+    console.log("")
+    console.log(query);
+    this.index.search({
+      query: query
+    },
+    function searchDone(err, content) {
+      if (err) throw err;
+  
+      console.log(content.hits);
+      console.log(content.hits[0]["description"]);
+      this.results = content.hits;
+    }.bind(this)
+  );
   }
 
 }
